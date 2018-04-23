1532b9598419482cbf0dcf7409f8556e12c55644
A. Akbar Hidayat <diqo@dicoding.com>
2018-04-23 09:47:34 +0700

    we don't require mcrypt anymore

--- src/Illuminate/Foundation/start.php
+++ src/Illuminate/Foundation/start.php
@@ -24,12 +24,12 @@ error_reporting(-1);
 |
 */
 
-if ( ! extension_loaded('mcrypt'))
-{
-	echo 'Mcrypt PHP extension required.'.PHP_EOL;
-
-	exit(1);
-}
+//if ( ! extension_loaded('mcrypt'))
+//{
+//	echo 'Mcrypt PHP extension required.'.PHP_EOL;
+//
+//	exit(1);
+//}
 
 /*
 |--------------------------------------------------------------------------
