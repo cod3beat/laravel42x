2e44db2f1a658935681176566a0b62c73b0c45a8
Ash-Shiddiqul Akbar Hidayat <diqo@dicoding.com>
2019-12-05 10:18:24 +0700

    migrated to prophecy

--- tests/Validation/ValidationFactoryTest.php
+++ tests/Validation/ValidationFactoryTest.php
@@ -1,53 +1,46 @@
 <?php
 
-use Mockery as m;
 use Illuminate\Validation\Factory;
 
 class ValidationFactoryTest extends \L4\Tests\BackwardCompatibleTestCase {
 
-	public function tearDown()
-	{
-		m::close();
-	}
-
-
 	public function testMakeMethodCreatesValidValidator()
 	{
-		$translator = m::mock('Symfony\Component\Translation\TranslatorInterface');
-		$factory = new Factory($translator);
+		$translator = $this->prophesize(Symfony\Contracts\Translation\TranslatorInterface::class);
+		$factory = new Factory($translator->reveal());
 		$validator = $factory->make(array('foo' => 'bar'), array('baz' => 'boom'));
-		$this->assertEquals($translator, $validator->getTranslator());
+		$this->assertEquals($translator->reveal(), $validator->getTranslator());
 		$this->assertEquals(array('foo' => 'bar'), $validator->getData());
 		$this->assertEquals(array('baz' => array('boom')), $validator->getRules());
 
-		$presence = m::mock('Illuminate\Validation\PresenceVerifierInterface');
+		$presence = $this->prophesize(Illuminate\Validation\PresenceVerifierInterface::class);
 		$noop1 = function() {};
 		$noop2 = function() {};
 		$noop3 = function() {};
 		$factory->extend('foo', $noop1);
 		$factory->extendImplicit('implicit', $noop2);
 		$factory->replacer('replacer', $noop3);
-		$factory->setPresenceVerifier($presence);
+		$factory->setPresenceVerifier($presence->reveal());
 		$validator = $factory->make(array(), array());
 		$this->assertEquals(array('foo' => $noop1, 'implicit' => $noop2), $validator->getExtensions());
 		$this->assertEquals(array('replacer' => $noop3), $validator->getReplacers());
-		$this->assertEquals($presence, $validator->getPresenceVerifier());
+		$this->assertEquals($presence->reveal(), $validator->getPresenceVerifier());
 
-		$presence = m::mock('Illuminate\Validation\PresenceVerifierInterface');
+		$presence = $this->prophesize(Illuminate\Validation\PresenceVerifierInterface::class);
 		$factory->extend('foo', $noop1, 'foo!');
 		$factory->extendImplicit('implicit', $noop2, 'implicit!');
-		$factory->setPresenceVerifier($presence);
+		$factory->setPresenceVerifier($presence->reveal());
 		$validator = $factory->make(array(), array());
 		$this->assertEquals(array('foo' => $noop1, 'implicit' => $noop2), $validator->getExtensions());
 		$this->assertEquals(array('foo' => 'foo!', 'implicit' => 'implicit!'), $validator->getFallbackMessages());
-		$this->assertEquals($presence, $validator->getPresenceVerifier());
+		$this->assertEquals($presence->reveal(), $validator->getPresenceVerifier());
 	}
 
 
 	public function testCustomResolverIsCalled()
 	{
 		unset($_SERVER['__validator.factory']);
-		$translator = m::mock('Symfony\Component\Translation\TranslatorInterface');
+		$translator = m::mock('Symfony\Contracts\Translation\TranslatorInterface');
 		$factory = new Factory($translator);
 		$factory->resolver(function($translator, $data, $rules)
 		{
