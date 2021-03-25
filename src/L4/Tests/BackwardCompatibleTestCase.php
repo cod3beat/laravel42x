ac32d8f9db258defc379b35dc29ed65ea56f7d56
Habibi Mustafa <bibitahabibi@gmail.com>
2021-03-25 10:46:51 +0700

    use prophecy trait

--- src/L4/Tests/BackwardCompatibleTestCase.php
+++ src/L4/Tests/BackwardCompatibleTestCase.php
@@ -2,10 +2,15 @@
 
 namespace L4\Tests;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 class BackwardCompatibleTestCase extends TestCase
 {
+    use ProphecyTrait;
+
     /**
      * Returns a mock object for the specified class.
      *
@@ -14,23 +19,23 @@ class BackwardCompatibleTestCase extends TestCase
      * We should update the code and remove this method but for now this is good enough.
      *
      *
-     * @param string     $originalClassName       Name of the class to mock.
+     * @param string $originalClassName Name of the class to mock.
      * @param array|null $methods                 When provided, only methods whose names are in the array
      *                                            are replaced with a configurable test double. The behavior
      *                                            of the other methods is not changed.
      *                                            Providing null means that no methods will be replaced.
-     * @param array      $arguments               Parameters to pass to the original class' constructor.
-     * @param string     $mockClassName           Class name for the generated test double class.
-     * @param bool       $callOriginalConstructor Can be used to disable the call to the original class' constructor.
-     * @param bool       $callOriginalClone       Can be used to disable the call to the original class' clone constructor.
-     * @param bool       $callAutoload            Can be used to disable __autoload() during the generation of the test double class.
-     * @param bool       $cloneArguments
-     * @param bool       $callOriginalMethods
-     * @param object     $proxyTarget
+     * @param array $arguments Parameters to pass to the original class' constructor.
+     * @param string $mockClassName Class name for the generated test double class.
+     * @param bool $callOriginalConstructor Can be used to disable the call to the original class' constructor.
+     * @param bool $callOriginalClone Can be used to disable the call to the original class' clone constructor.
+     * @param bool $callAutoload Can be used to disable __autoload() during the generation of the test double class.
+     * @param bool $cloneArguments
+     * @param bool $callOriginalMethods
+     * @param object $proxyTarget
      *
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @return PHPUnit_Framework_MockObject_MockObject
      *
-     * @throws \Exception
+     * @throws Exception
      */
     public function getMock($originalClassName, $methods = array(), array $arguments = array(), $mockClassName = '', $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true, $cloneArguments = false, $callOriginalMethods = false, $proxyTarget = null)
     {
