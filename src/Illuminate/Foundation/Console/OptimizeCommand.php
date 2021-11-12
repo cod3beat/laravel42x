50a7abc47bfb69bd77ff3ff717402c70a55415f1
Habibi Mustafa <bibitahabibi@gmail.com>
2021-11-12 14:22:48 +0700

    not needed to compile class

--- src/Illuminate/Foundation/Console/OptimizeCommand.php
+++ src/Illuminate/Foundation/Console/OptimizeCommand.php
@@ -3,7 +3,6 @@
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Composer;
 use Illuminate\View\Engines\CompilerEngine;
-use ClassPreloader\Console\PreCompileCommand;
 use Symfony\Component\Console\Input\InputOption;
 
 class OptimizeCommand extends Command {
@@ -62,10 +61,6 @@ class OptimizeCommand extends Command {
 
 		if ($this->option('force') || ! $this->laravel['config']['app.debug'])
 		{
-			$this->info('Compiling common classes');
-
-			$this->compileClasses();
-
 			$this->info('Compiling views');
 
 			$this->compileViews();
@@ -74,48 +69,6 @@ class OptimizeCommand extends Command {
 		{
 			$this->call('clear-compiled');
 		}
-	}
-
-	/**
-	 * Generate the compiled class file.
-	 *
-	 * @return void
-	 */
-	protected function compileClasses()
-	{
-		$this->registerClassPreloaderCommand();
-
-		$outputPath = $this->laravel['path.base'].'/bootstrap/compiled.php';
-
-		$this->callSilent('compile', array(
-			'--config' => implode(',', $this->getClassFiles()),
-			'--output' => $outputPath,
-			'--strip_comments' => 1,
-		));
-	}
-
-	/**
-	 * Get the classes that should be combined and compiled.
-	 *
-	 * @return array
-	 */
-	protected function getClassFiles()
-	{
-		$app = $this->laravel;
-
-		$core = require __DIR__.'/Optimize/config.php';
-
-		return array_merge($core, $this->laravel['config']['compile']);
-	}
-
-	/**
-	 * Register the pre-compiler command instance with Artisan.
-	 *
-	 * @return void
-	 */
-	protected function registerClassPreloaderCommand()
-	{
-		$this->getApplication()->add(new PreCompileCommand);
 	}
 
 	/**
